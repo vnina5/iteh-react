f823191e18826b256e7027e968228e0f9f1efe1c
Nikolina Vrankovic <vrankovic.nina00@gmail.com>
2023-02-09 01:27:55 +0100

    napravljen footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react";
+import { FaHeart } from "react-icons/fa";
 
 function Footer() {
   return (
     <div className="footer">
-        <h1>OVO JE FUTERRR</h1>
-        <p>tu ce biti kontakt!!!</p>
+      <h3>
+        Made with <FaHeart className="love" />
+      </h3>
+      {/* <br />
+      <h4>by Nina</h4> */}
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
